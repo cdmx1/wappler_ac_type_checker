ebfa166a90a5d63cd12f6eb925e196071deb1680
Roney Dsilva <roney.dsilva@cdmx.in>
2023-09-20 14:49:29 +0530

    initial commit

---
+++ dmx-type-checker-formatter.js
@@ -0,0 +1,39 @@
+dmx.Formatters('any', {
+    isNumber: function(input) {
+      return !isNaN(parseFloat(input)) && isFinite(input);
+    },
+    isUUID: function(input) {
+      const uuidRegex = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
+      return uuidRegex.test(input);
+    },
+    isBool: function(input) {
+      return typeof input === 'boolean';
+    },
+    isString: function(input) {
+      return typeof input === 'string';
+    },
+    isArray: function(input) {
+      return Array.isArray(input);
+    },
+    isObject: function(input) {
+      return typeof input === 'object' && !Array.isArray(input) && input !== null;
+    },
+    isFunction: function(input) {
+      return typeof input === 'function';
+    },
+    isNull: function(input) {
+      return input === null;
+    },
+    isUndefined: function(input) {
+      return typeof input === 'undefined';
+    },
+    isDate: function(input) {
+      return input instanceof Date;
+    },
+    isRegExp: function(input) {
+      return input instanceof RegExp;
+    },
+    isNaN: function(input) {
+      return Number.isNaN(input);
+    }
+  });
